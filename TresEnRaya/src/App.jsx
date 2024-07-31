38398a4a0f928ad0d575fb885cdfc3f0204e3035
espadv69 <aitorjob69@gmail.com>
2024-07-31 21:07:41 +0200

    Update: Advances in my game

--- TresEnRaya/src/App.jsx
+++ TresEnRaya/src/App.jsx
@@ -1,26 +1,36 @@
+import { Children } from 'react'
 import './App.css'
 
-const TURNS = {
+const TURNS = { //Esta constante TURNS guarda quiénes pueden jugar: 'X' y 'O'. En un juego de tres en raya, los jugadores toman turnos usando 'X' o 'O'.
   X: 'x',
   O: 'o'
 }
 
-const board = Array(9).fill(null)
+
+
+const Square = ({ children, updateBoard, index }) => { //Aquí tenemos un componente llamado Square que representa un cuadrado del tablero de tres en raya.
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
 
 function App() {
-
+  const board = Array(9).fill(null) //Es un arreglo que tiene 9 espacios, todos inicializados con null. Representa nuestro tablero de tres en raya vacío.
   return (
     <main className="board">
       <h2>Hola mundo</h2>
       <section className="game">
         {
-          board.map((_, index) => {
+          board.map((_, index) => { {/* Este es un método que recorre cada espacio del tablero (9 en total) y crea un componente Square para cada uno. Usa index para identificar cada cuadrado. */}
             return (
-              <div className="cell" key={index}>
-                <span className="cell__content">
-                  {index}
-                </span>
-              </div>
+              <Square 
+                key={index}
+                index={index}
+              >
+                {index}
+              </Square>
             )
           }) 
         }
